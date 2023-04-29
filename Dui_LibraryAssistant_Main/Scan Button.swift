e0fc13121840b495767b58f3518baf9f1fe4f662
Vhagar343 <89761148+Vhagar343@users.noreply.github.com>
2023-04-29 03:29:44 -0600

    Found a good image scanner for what we need, slowly adapting it for our needs

---
+++ Dui_LibraryAssistant_Main/Scan Button.swift
@@ -0,0 +1,50 @@
+//
+//  Scan Button.swift
+//  Dui_LibraryAssistant_Main
+//
+//  Created by Allison Dalton on 4/29/23.
+//
+
+import SwiftUI
+
+struct ScanButton: UIViewRepresentable {
+  @Binding var text: String
+  @Binding var title: String
+
+  func makeUIView(context: Context) -> UIButton {
+    let textFromCamera = UIAction.captureTextFromCamera(
+      responder: context.coordinator,
+      identifier: nil)
+    let button = UIButton()
+    button.setImage(
+      UIImage(systemName: "camera.badge.ellipsis"),
+      for: .normal)
+    button.menu = UIMenu(children: [textFromCamera])
+    return button
+  }
+
+  func updateUIView(_ uiView: UIButton, context: Context) { }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+
+  class Coordinator: UIResponder, UIKeyInput {
+    let parent: ScanButton
+    init(_ parent: ScanButton) { self.parent = parent }
+
+    var hasText = false
+    func insertText(_ text: String) {
+      parent.text = text
+      parent.title = "Add \(text)"
+    }
+    func deleteBackward() { }
+  }
+}
+
+struct ScanButton_Previews: PreviewProvider {
+  static var previews: some View {
+    ScanButton(text: .constant(""), title: .constant(""))
+      .previewLayout(.sizeThatFits)
+  }
+}
---
+++ Dui_LibraryAssistant_Main/ScanBookView.swift
@@ -0,0 +1,22 @@
+//
+//  ScanBookView.swift
+//  Dui_LibraryAssistant_Main
+//
+//  Created by Allison Dalton on 4/29/23.
+//
+
+import SwiftUI
+
+struct ScanBookView: View {
+    @State private var Scanned = ""
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScanBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScanBookView()
+    }
+}
