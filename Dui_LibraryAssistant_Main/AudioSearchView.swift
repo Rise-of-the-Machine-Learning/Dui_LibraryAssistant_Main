d4fe60847b9d865bd4906ec5cb11c7f0d7653121
Salizan (Aria) <97919315+Salizn@users.noreply.github.com>
2023-04-29 23:11:37 -0600

    Speech Recognition Implemented

--- Dui_LibraryAssistant_Main/AudioSearchView.swift
+++ Dui_LibraryAssistant_Main/AudioSearchView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct AudioSearchView: View {
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
     var body: some View {
         Text("Audio Search")
+        VStack {
+            Text(speechRecognizer.transcript)
+                .padding()
+            
+            Button(action: {
+                if !isRecording {
+                    speechRecognizer.transcribe()
+                } else {
+                    speechRecognizer.stopTranscribing()
+                }
+                
+                isRecording.toggle()
+            }) {
+                Text(isRecording ? "Stop" : "Record")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(isRecording ? Color.red : Color.blue)
+                    .cornerRadius(10)
+            }
+        }
     }
 }
 
